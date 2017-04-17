e5fff43e7e61c815467c12cb40d9fa43f57a5b42
Klaus <KlausSchwartz@gmail.com>
2017-04-17 14:08:36 +0200

    added grayscale template. with minimal modifications

--- src/main/java/de/zahori/FrontendController.java
+++ src/main/java/de/zahori/FrontendController.java
@@ -1,14 +1,17 @@
 package de.zahori;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FrontendController {
 
     
     @RequestMapping("/")
-    public String index() {
+    public String index(@RequestParam(value="title", required=false, defaultValue="Grayscale") String title, Model model) {
+    	model.addAttribute("title", title);
         return "index";
     }
 	
